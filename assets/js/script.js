bee96816619d5a70a3711204aff26cd204c20fd6
ImalikCo <mohammed.ibragimov97@gmail.com>
2024-02-01 17:29:14 +0100

    added first question (no styling yet)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
 const startBtn = document.querySelector('.start-btn');
 const popupInfo = document.querySelector('.popup-info');
 const exitBtn = document.querySelector('.exit-btn');
+const main = document.querySelector('.main');
+const continueBtn = document.querySelector('.continue-btn');
+const quizSeciton = document.querySelector('.quiz-section');
 
 startBtn.onclick = () =>{
     popupInfo.classList.add('active');
+    main.classList.add('active');
 }
 
 exitBtn.onclick = () =>{
     popupInfo.classList.remove('active');
+    main.classList.remove('active');
+}
+
+continueBtn.onclick = () =>{
+    quizSeciton.classList.add('active');
+    popupInfo.classList.remove('active');
+    main.classList.remove('active');
 }
