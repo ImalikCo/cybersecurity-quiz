3f6a121ed9d0f51f75a5fbad36deb15f6dd8eaa3
ImalikCo <mohammed.ibragimov97@gmail.com>
2024-02-05 13:37:49 +0100

    added correct and wrong questions with appurtenant colours

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,70 @@ continueBtn.onclick = () =>{
     popupInfo.classList.remove('active');
     main.classList.remove('active');
     quizBox.classList.add('active');
+    showQuestions(0)
+
 }
 
 let questionsCount = 0;
+let questionsNumb = 1;
+
+const nextBtn = document.querySelector('.next-btn');
+
+nextBtn.onclick = () => {
+    if (questionsCount < questions.length -1){
+        questionsCount++;
+        showQuestions(questionsCount)
+        questionsNumb++;
+        questionCounter(questionsNumb);
+    }
+    else {
+        console.log('Question completed');
+    }
+}
+
+const optionList = document.querySelector('.option-list')
+
+function showQuestions(index) {
+    const questionText = document.querySelector('.question-text');
+    questionText.textContent = `${questions[index].numb}. ${questions[index].question}`;
+
+    let optionTag = `<div class="option"><span>${questions[index].options[0]}</span></div>
+    <div class="option"><span>${questions[index].options[1]}</span></div>
+    <div class="option"><span>${questions[index].options[2]}</span></div>
+    <div class="option"><span>${questions[index].options[3]}</span></div>`;
+
+    optionList.innerHTML = optionTag;
+
+    const option = document.querySelectorAll('.option');
+    for (let i = 0; i < option.length; i++) {
+        option[i].setAttribute('onclick', 'optionSelected(this)');
+    }
+
+}
+
+function optionSelected(answer){
+    let userAnswer = answer.textContent;
+    let correctAnswer = questions[questionsCount].answer;
+    let allOptions = optionList.children.length;
+    if (userAnswer == correctAnswer) {
+        answer.classList.add('correct');
+    }
+    else {
+        answer.classList.add('incorrect');
+
+        for (let i = 0; i < allOptions; i++) {
+            if (optionList.children[i].textContent == correctAnswer){
+                optionList.children[i].setAttribute('class', 'option correct');
+            }
+        }
+    }
+
+    for (let i = 0; i < allOptions; i++){
+        optionList.children[i].classList.add('disabled');
+    }
+}
+
+function questionCounter(index) {
+    const questionTotal = document.querySelector('.question-total');
+    questionTotal.textContent = `${index} of ${questions.length} Questions`
+}
